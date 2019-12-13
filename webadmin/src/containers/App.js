7b986e2a01f97f6b28bc29a009c8c27fbd383846
Minh_Sang <sang.nguyen@terralogic.com>
2019-12-13 11:57:40 +0700

    up code

---
+++ webadmin/src/containers/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Login from './Login/Login';
+
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+        <Login/>
+      </div>
+    );
+  }
+}
+
+export default App;
